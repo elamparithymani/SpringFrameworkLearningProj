b4e5928ba2cd3d67c07bb7a81c9831f9f3a1f87c
elamparithy <elamparithy888@@gmail.com>
2022-08-20 13:44:58 +0530

    Initial commit

--- src/main/java/SpringLearningProj/Main.java
+++ src/main/java/SpringLearningProj/Main.java
@@ -3,7 +3,7 @@ package SpringLearningProj;
 public class Main {
 
     public static void main(String args[]) {
-        System.out.println("Spring Learning");
+        System.out.println("Spring Learning ");
     }
 
 }
